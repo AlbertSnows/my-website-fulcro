7d7cf3f1353c92081deee59035e046444c609d74
Albert Snow <ajsnow2012@gmail.com>
2020-10-07 22:45:30 -0500

    trying to get external state working

--- src/main/app/ui/mutations.cljs
+++ src/main/app/ui/mutations.cljs
@@ -1,9 +1,16 @@
 (ns app.ui.mutations
   (:require
+    [com.fulcrologic.fulcro.algorithms.normalized-state :refer [swap!->]]
     [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]))
 
-(defmutation toggle [{:button/keys [id]}]
-  (action [{:keys [state]}]
-    (swap! state update-in
-           [:button/id id :button/num]
-           #(if (= %1 1) 0 1))))
+(defmutation toggle [{:keys [id]}]
+  (action [{:keys [state] :as env}]
+      (swap!-> state
+         (update-in [:button/id id :button/num]
+                    #(if (= %1 1) 0 1))
+         (update-in [:sidebar-contents/id id :sidebar-contents/state]
+                    #(if (= %1 1) 0 1)))
+    ;(swap! state update-in [:button/id id :button/num]
+    ;    #(if (= %1 1) 0 1))
+          )
+  )

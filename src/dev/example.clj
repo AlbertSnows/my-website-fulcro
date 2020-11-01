bb19ef88f60a18066e8dcb77833d83ed7c59cff2
Albert Snow <ajsnow2012@gmail.com>
2020-10-31 22:55:49 -0500

    working on about page

--- src/dev/example.clj
+++ src/dev/example.clj
@@ -9,6 +9,119 @@
     [com.fulcrologic.fulcro-css.localized-dom :as dom
      :refer [div label button span]]))
 
+;(def image-dir "../images/")
+;
+;(def classes "brief-border href-item-container")
+;
+;(def project-page-header
+;  [:p {:id "project-header"} "Projects"])
+;
+;(defn box-container [contents]
+;  [:div {:class "flex box align-horizontal"} contents])
+;
+;(defn image-container [src alt]
+;  [:img {:src src :alt alt}])
+;
+;(defn image-href-container [id class href src alt]
+;  [:div {:id id :class class}
+;   [:a {:href href :target "__blank" :rel "noopener noreferrer"}
+;    (image-container src alt)]])
+;
+;(defn project-description [title description]
+;  [:div {:class "description-container"}
+;   [:div {:class "text-align-left"} title]
+;   [:div {:class "description text-align-left"} description]])
+;
+;(def project-page-body
+;  [:div {:id "project-body"}
+;   (box-container
+;     [:div (image-href-container
+;             "my-website"
+;             classes
+;             "https://github.com/AlbertSnows/my-website"
+;             (str image-dir "this_website.PNG")
+;             "This Website")
+;      (project-description
+;        "This Website"
+;        "The website you're perusing")])
+;   (box-container
+;     [:div (image-href-container
+;             "first-website"
+;             classes
+;             "https://github.com/AlbertSnows/FWRcljs"
+;             (str image-dir "kistners_flowers.PNG")
+;             "My First Website")
+;      (project-description
+;        "My First website"
+;        "First website for class.
+;         Written from Javascript
+;         -> Typescript
+;         -> Clojurescript over the semester")])
+;   (box-container
+;     [:div (image-href-container
+;             "snake-game"
+;             classes
+;             "https://github.com/AlbertSnows/snake_game"
+;             (str image-dir "snake_rust.PNG")
+;             "Snake Game")
+;      (project-description "Snake Game" "Wrote Snake in Rust")])
+;   (box-container
+;     [:div (image-href-container
+;             "game-jam-2018"
+;             classes
+;             "https://github.com/AlbertSnows/To-Change-A-Lightbulb"
+;             (str image-dir "lightbulb.PNG")
+;             "To Change A Light Bulb")
+;      (project-description
+;        "To Change A Light Bulb"
+;        "A beautiful disaster written over a weekend during a game jam")])
+;   (box-container
+;     [:div (image-href-container
+;             "thermal-modeling"
+;             classes
+;             "https://github.com/AlbertSnows/HumanThermalModeling"
+;             (str image-dir "thermal_modeling.PNG")
+;             "Human Thermal Modeling")
+;      (project-description
+;        "Human Thermal Modeling"
+;        "Multi-threading graduate student's research code")])
+;   (box-container
+;     [:div (image-href-container
+;             "roguelike"
+;             classes
+;             "https://github.com/AlbertSnows/2DRogueLike"
+;             (str image-dir "2Drouge.PNG")
+;             "2D Rogue Like")
+;      (project-description "2D Rogue Like" "First game made in Unity")])
+;   (box-container
+;     [:div (image-href-container
+;             "edgesweeper"
+;             classes
+;             "https://github.com/AlbertSnows/python-tkinter-minesweeper"
+;             (str image-dir "minesweeper.PNG")
+;             "Edgesweeper")
+;      (project-description
+;        "Edgesweeper"
+;        "Took Minesweeper, written in python, and added a feature")])
+;   (box-container
+;     [:div (image-href-container
+;             "first-unity"
+;             classes
+;             "https://github.com/AlbertSnows/RollABall"
+;             (str image-dir "rollaball.PNG")
+;             "Roll A Ball")
+;      (project-description "Roll A Ball" "First tutorial exposure to Unity")])
+;   (box-container
+;     [:div (image-href-container
+;             "mobile-app-game"
+;             classes
+;             "https://github.com/AlbertSnows/SimpleMobileGame"
+;             (str image-dir "first_app.PNG")
+;             "Simple App")
+;      (project-description
+;        "Simple App"
+;        "Playing with Java/Kotlin in Android Studio")])])
+
 ;(defsc Projects [this {:projects/keys [id] :as props}]
 ;  {:query [:projects/id]
 ;   :route-segment ["projects"]

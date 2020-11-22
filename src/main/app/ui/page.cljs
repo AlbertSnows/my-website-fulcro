41640aa3e4725b4b4c1deedc2f6ffce3f3c3f971
Albert Snow <ajsnow2012@gmail.com>
2020-11-22 14:28:45 -0600

    getting header to respect route

--- src/main/app/ui/page.cljs
+++ src/main/app/ui/page.cljs
@@ -17,16 +17,17 @@
      :refer [defrouter]]
     [com.fulcrologic.fulcro.ui-state-machines :as uism
      :refer [defstatemachine]]
-    [app.ui.css :as uicss]))
+    [app.ui.css :as uicss]
+    [taoensso.timbre :as log]
+    ))
 
 (defrouter RootRouter
-           [this {:keys [current-state pending-path-segment]}]
-           {:router-targets [h/Home a/About c/Contact p/Projects]})
+  [this {:keys [current-state pending-path-segment]}]
+  {:router-targets [h/Home a/About c/Contact p/Projects]})
 (def ui-root-router (factory RootRouter))
 
-(defsc OuterBox [this {:outer/keys [id route header router] :as props}]
+(defsc OuterBox [this {:outer/keys [id router] :as props}]
   {:query [:outer/id
-           :outer/route
            :outer/header
            {:outer/router (get-query RootRouter)}
            [::uism/asm-id ::RootRouter]]
@@ -37,60 +38,55 @@
    :ident :outer/id
    :initial-state
           (fn [{:outer/keys [id route] :as params}]
-            {:outer/id    id
-             :outer/route "contact"
-             :outer/header
-                          (get-initial-state
-                            ContainerHeader
-                            {:container-header/id    id
-                             :container-header/route "home"})
-             :outer/router
-                          (get-initial-state RootRouter {})})
+            {:outer/id     id
+             :outer/router (get-initial-state RootRouter {})})
    :css   uicss/OuterBox}
   (let [{:keys [outer box]} (get-classnames OuterBox)]
-    (div
-      {:nonsense "TURN BACK, YE WHO ENTER THE DOMAIN OF HTML"
-       :classes  [outer]}
-      (ui-container-header header)
-      (div
-        {:classes [box]}
-        (ui-root-router router)))))
+    (div {:nonsense "TURN BACK, YE WHO ENTER THE DOMAIN OF HTML"
+          :classes  [outer]}
+         (ui-container-header
+           (get-initial-state
+             ContainerHeader
+             {:container-header/id    id
+              :container-header/route (first (dr/current-route this))}))
+         (div {:classes [box]}
+              (ui-root-router router)))))
 (def ui-outer (factory OuterBox))
 
 (defsc Page [this {:page/keys [outer sidebar]}]
   {:query [{:page/outer (get-query OuterBox)}
            {:page/sidebar (get-query Sidebar)}]
    :initial-state
-    (fn [_] {:page/outer
-             (get-initial-state
-               OuterBox {:outer/id    1
-                         :outer/route "home"})
-             :page/sidebar
-             (get-initial-state
-               Sidebar {:sidebar/id 1})})
-   :css [[:.page
-          {:display         "flex"
-           :align-items     "center"
-           :justify-content "center"}]]}
-       (let [{:keys [page]} (get-classnames Page)]
-            (div {:classes [page]}
-                     (ui-outer outer)
-                     (ui-sidebar sidebar))))
+          (fn [_] {:page/outer
+                   (get-initial-state
+                     OuterBox {:outer/id    1
+                               :outer/route "home"})
+                   :page/sidebar
+                   (get-initial-state
+                     Sidebar {:sidebar/id 1})})
+   :css   [[:.page
+            {:display         "flex"
+             :align-items     "center"
+             :justify-content "center"}]]}
+  (let [{:keys [page]} (get-classnames Page)]
+    (div {:classes [page]}
+         (ui-outer outer)
+         (ui-sidebar sidebar))))
 (def ui-page (factory Page))
 
 (defsc Root [this {:root/keys [page] :as props}]
   {:query [{:root/page (get-query Page)}]
    :initial-state
-    (fn [_] {:root/page
-              (get-initial-state Page)})
-   :css [[:.container
-          {:display "flex"
-           :align-items "center"
-           :justify-content "center"}]]}
-       (let [{:keys [container]} (get-classnames Root)]
-            (div
-              {:classes [container]}
-              (style-element {:component Root})
-              (style-element {:component h/Home})
-              (style-element {:component ContainerHeader})
-              (ui-page page))))
+          (fn [_] {:root/page
+                   (get-initial-state Page)})
+   :css   [[:.container
+            {:display         "flex"
+             :align-items     "center"
+             :justify-content "center"}]]}
+  (let [{:keys [container]} (get-classnames Root)]
+    (div
+      {:classes [container]}
+      (style-element {:component Root})
+      (style-element {:component h/Home})
+      (style-element {:component ContainerHeader})
+      (ui-page page))))

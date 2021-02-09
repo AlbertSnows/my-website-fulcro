88c15557dc0c5a39e00e31166ddc151b468d7451
Albert Snow <ajsnow2012@gmail.com>
2021-02-08 23:38:38 -0600

    rename? p2

--- src/main/app/ui/pages/projects.cljs
+++ src/main/app/ui/pages/projects.cljs
@@ -49,7 +49,7 @@
 			:box/href
 							{:href/link  "https://github.com/AlbertSnows/my-website-fulcro"
 							 :href/image {:image/alt "This Website...In Fulcro"
-														:image/src "../images/this_website.PNG"}}})
+														:image/src "../images/this_website_f.PNG"}}})
 
 	 (build-project-box
 		 {:box/id "my-website"

dc3501b713ebb93efd98caa4c9f628dc5e1ee26f
Albert Snow <ajsnow2012@gmail.com>
2022-02-04 19:07:57 -0600

    update

--- src/main/app/setup/server_main.clj
+++ src/main/app/setup/server_main.clj
@@ -3,8 +3,9 @@
     [mount.core :as mount]
     app.server-components.http-server)
   (:gen-class))
+; npx shadow-cljs release main
 ; prod command: clojure -X:depstar uberjar :aot true :jar fulcro.jar :main-class app.setup.server-main
 ; java -jar fulcro.jar
-;; This is a separate file for the uberjar only. We control the server in dev mode from src/dev/user.clj
+; This is a separate file for the uberjar only. We control the server in dev mode from src/dev/user.clj
 (defn -main [& args]
   (mount/start-with-args {:config "config/prod.edn"}))
